d4073ae8ad8a8c961937a9277f81970fe30ec624
monilghori <monilghori46@gmail.com>
2024-06-20 17:26:13 +0530

    remove error of document is not define.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,40 @@
-import Grid from "@/Components/Grid";
-import WelcomeTitle from "@/Components/WelcomeTitle";
+'use client'
+import dynamic from 'next/dynamic';
+//import Grid from "@/Components/Grid";
+//import WelcomeTitle from "@/Components/WelcomeTitle";
 import { FloatingNav } from "@/Components/ui/Navbar";
 import { navItems } from "../../Data";
-import RecentProjects from "@/Components/RecentProjects";
-import Approach from "@/Components/Approach";
-import Footer from "@/Components/Footer";
+//import RecentProjects from "@/Components/RecentProjects";
+//import Approach from "@/Components/Approach";
+//import Footer from "@/Components/Footer";
+
+const Grid = dynamic(() => import('@/Components/Grid'), {
+  ssr: false, // This disables SSR for this component
+});
+const WelcomeTitle = dynamic(() => import('@/Components/WelcomeTitle'), {
+  ssr: false, // This disables SSR for this component
+});
+const RecentProjects = dynamic(() => import('@/Components/RecentProjects'), {
+  ssr: false, // This disables SSR for this component
+});
+const Approach = dynamic(() => import('@/Components/Approach'), {
+  ssr: false, // This disables SSR for this component
+});
+const Footer = dynamic(() => import('@/Components/Footer'), {
+  ssr: false, // This disables SSR for this component
+});
 export default function Home() {
-  const isBrowser = typeof document !== 'undefined';
+
   return (
     <main className="bg-black-100 flex justify-center items-center relative flex-col overflow-clip">
       <div className="max-w-7xl w-full text-white">
         <FloatingNav navItems={navItems} />
         <div className="flex flex-col">
-        {isBrowser && <WelcomeTitle />}
-        {isBrowser && <Grid />}
-        {isBrowser &&  <RecentProjects />}
-        {isBrowser &&  <Approach />}
-        {isBrowser &&  <Footer />}
+        <WelcomeTitle />
+        <Grid />
+        <RecentProjects />
+        <Approach />
+        <Footer />
         </div>
       </div>
     </main>
